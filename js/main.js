e0ed359db4d2c2bc2182d88a5c1e8c5306114719
nazmul37290 <nazmulnjm@gmail.com>
2024-06-28 12:59:25 +0600

    added faq section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ function showTime() {
 }
 
 setInterval(showTime, 1000);
+
+// faq related scripts
+
+const questions = document.getElementsByClassName("question");
+console.log(questions);
+for (const question of questions) {
+  question.addEventListener("click", function () {
+    const answer = question.nextElementSibling;
+    console.log(answer);
+    answer.classList.toggle("hidden");
+  });
+}
